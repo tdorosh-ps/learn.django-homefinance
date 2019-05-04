ad47109e4838c781738dcfc8fa5590908da165d3
tdorosh <tdtechmail@ukr.net>
2019-05-04 12:12:56 +0300

    add curency model

--- finance/models.py
+++ finance/models.py
@@ -22,4 +22,54 @@ class Transaction(models.Model):
 		
 		
 class Account(models.Model):
+	title = model.CharField('Назва', max_length=255)
+	amount = model.DecimalField('Залишок', max_digits=11, decimal_places=2, default='00000,00')
+	notes = models.TextField()
+	create_datetime = models.DateTimeField(default=timezone.now)
+	is_default = model.BooleanField('За замовчуванням')
+	
+	class Meta(object):
+		ordering = ['amount']
+		verbose_name = 'Рахунок'
+		verbose_name_plural = 'Рахунки'
+		
+	def __str__(self):
+		return '{}, {}'.format(self.title, self.amount)
+		
+class Currency(models.Model):
+	name = model.CharField('Назва', max_length=5)
+	full_name = model.CharField('Повна назва', max_length=100)
+	is_default = model.BooleanField('За замовчуванням')
+	
+	class Meta(object):
+		ordering = ['name']
+		verbose_name = 'Валюта'
+		verbose_name_plural = 'Валюти'
+		
+	def __str__(self):
+		return '{}, {}'.format(self.full_name, self.name)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
