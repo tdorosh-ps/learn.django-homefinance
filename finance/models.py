e949f08ae5e363be9a5aa96e8ce9f39e4a70f81a
tdorosh <tdtechmail@ukr.net>
2019-05-04 11:46:40 +0300

    add transaction model

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Transaction(models.Model):
+	amount = models.DecimalField('Сума', max_digits=11, decimal_places=2, default='00000,00')
+	currency = models.ForeignKey('Currency', verbose_name='Валюта', on_delete=models.PROTECT, default=lambda: Currency.objects.get(is_default=True)
+	type = models.ForeignKey('Type', verbose_name='Тип', on_delete=models.PROTECT)
+	category = models.ForeignKey('Category', verbose_name='Категорія', on_delete=models.PROTECT, limit_choices_to={})
+	subcategory = models.ForeignKey('Subcategory', verbose_name='Підкатегорія', on_delete=models.PROTECT, limit_choices_to={})
+	from_account = models.ForeignKey('Account', verbose_name='З рахунку', on_delete=models.PROTECT)
+	on_account = models.ForeignKey('Account', verbose_name='На рахунок', on_delete=models.PROTECT)
+	date_time = models.DateTimeField(default=timezone.now)
+	notes = models.TextField()
+	
+	class Meta(object):
+		ordering = ['-date_time']
+		verbose_name = 'Трансакція'
+		verbose_name_plural = 'Трансакції'
+		
+	def __str__(self):
+		return '{}, {} {}, {}'.format(self.notes, self.amount, self.currency, self.date_time)
--- finance/views.py
+++ finance/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+#from .models import Transaction
 
 # Create your views here.
 def transactions_list(request):
