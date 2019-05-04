9455649f23e472c808e55799959b6b8565d4e154
tdorosh <tdtechmail@ukr.net>
2019-05-04 12:23:14 +0300

    add type model

--- finance/models.py
+++ finance/models.py
@@ -3,14 +3,14 @@ from django.utils import timezone
 # Create your models here.
 class Transaction(models.Model):
 	amount = models.DecimalField('Сума', max_digits=11, decimal_places=2, default='00000,00')
-	currency = models.ForeignKey('Currency', verbose_name='Валюта', on_delete=models.PROTECT, default=lambda: Currency.objects.get(is_default=True)
-	type = models.ForeignKey('Type', verbose_name='Тип', on_delete=models.PROTECT)
-	category = models.ForeignKey('Category', verbose_name='Категорія', on_delete=models.PROTECT, limit_choices_to={})
-	subcategory = models.ForeignKey('Subcategory', verbose_name='Підкатегорія', on_delete=models.PROTECT, limit_choices_to={})
+	currencies = models.ForeignKey('Currency', verbose_name='Валюта', on_delete=models.PROTECT, default=lambda: Currency.objects.get(is_default=True)
+	types = models.ForeignKey('Type', verbose_name='Тип', on_delete=models.PROTECT)
+	categories = models.ForeignKey('Category', verbose_name='Категорія', on_delete=models.PROTECT, limit_choices_to={})
+	subcategories = models.ForeignKey('Subcategory', verbose_name='Підкатегорія', on_delete=models.PROTECT, limit_choices_to={})
 	from_account = models.ForeignKey('Account', verbose_name='З рахунку', on_delete=models.PROTECT)
 	on_account = models.ForeignKey('Account', verbose_name='На рахунок', on_delete=models.PROTECT)
-	create_datetime = models.DateTimeField(default=timezone.now)
-	notes = models.TextField()
+	create_datetime = models.DateTimeField('Дата здійснення', default=timezone.now)
+	notes = models.TextField('Додаткові відомості')
 	
 	class Meta(object):
 		ordering = ['-date_time']
@@ -24,8 +24,8 @@ class Transaction(models.Model):
 class Account(models.Model):
 	title = model.CharField('Назва', max_length=255)
 	amount = model.DecimalField('Залишок', max_digits=11, decimal_places=2, default='00000,00')
-	notes = models.TextField()
-	create_datetime = models.DateTimeField(default=timezone.now)
+	notes = models.TextField('Додаткові відомості')
+	create_datetime = models.DateTimeField('Дата створення', default=timezone.now)
 	is_default = model.BooleanField('За замовчуванням')
 	
 	class Meta(object):
@@ -42,14 +42,22 @@ class Currency(models.Model):
 	is_default = model.BooleanField('За замовчуванням')
 	
 	class Meta(object):
-		ordering = ['name']
 		verbose_name = 'Валюта'
 		verbose_name_plural = 'Валюти'
 		
 	def __str__(self):
 		return '{}, {}'.format(self.full_name, self.name)
+		
+class Type(model.Models):
+	name = model.CharField('Назва', max_length=100)
+	categories = models.ForeignKey('Category', verbose_name='Категорії', on_delete=models.PROTECT)
 	
-	
+	class Meta(object):
+		verbose_name = 'Тип'
+		verbose_name_plural = 'Типи'
+		
+	def __str__(self):
+		return '{}'.format(self.name)
 	
 	
 	
