47e079f3d4149fbed98c02ea71c6775348e7f194
tdorosh <tdtechmail@ukr.net>
2019-05-19 11:17:07 +0300

    create first Accoutns GBV views

--- finance/urls.py
+++ finance/urls.py
@@ -19,5 +19,46 @@ urlpatterns = [
 	path('accounting/transactions/<int:pk>/edit/', views.TransactionEditView.as_view(), name='transaction_edit'),
 	path('accounting/transactions/<int:pk>/delete/', views.TransactionDeleteView.as_view(), name='transaction_delete'),
 	
+		#Account urls
+	path('accounting/accounts/', views.AccountsListView.as_view(), name='accounts_list'),
+	path('accounting/accounts/<int:pk>/', views.AccountDetailView.as_view(), name='account_detail'),
+	path('accounting/accounts/create/', views.AccountCreateView.as_view(), name='account_create'),
+	path('accounting/accounts/<int:pk>/edit/', views.AccountEditView.as_view(), name='account_edit'),
+	path('accounting/accounts/<int:pk>/delete/', views.AccountDeleteView.as_view(), name='account_delete'),
 	
+		#Currencies urls
+	path('accounting/currencies/', views.CurrenciesListView.as_view(), name='currencies_list'),
+	path('accounting/currency/create/', views.CurrencyCreateView.as_view(), name='currency_create'),
+	path('accounting/currency/<int:pk>/edit/', views.CurrencyEditView.as_view(), name='currency_edit'),
+	path('accounting/currency/<int:pk>/delete/', views.CurrencyDeleteView.as_view(), name='currency_delete'),
+	
+		#Types urls
+	path('accounting/types/', views.TypesListView.as_view(), name='types_list'),
+	path('accounting/type/create/', views.TypeCreateView.as_view(), name='type_create'),
+	path('accounting/type/<int:pk>/edit/', views.TypeEditView.as_view(), name='type_edit'),
+	path('accounting/type/<int:pk>/delete/', views.TypeDeleteView.as_view(), name='type_delete'),
+	
+		#Category urls
+	path('accounting/categories/', views.CategoriesListView.as_view(), name='categories_list'),
+	path('accounting/category/create/', views.CategoryCreateView.as_view(), name='category_create'),
+	path('accounting/category/<int:pk>/edit/', views.CategoryEditView.as_view(), name='category_edit'),
+	path('accounting/category/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category_delete'),
+	
+		#Subcategories urls
+	path('accounting/subcategories/', views.SubcategoriesListView.as_view(), name='subcategories_list'),
+	path('accounting/subcategory/create/', views.SubcategoryCreateView.as_view(), name='subcategory_create'),
+	path('accounting/subcategory/<int:pk>/edit/', views.SubcategoryEditView.as_view(), name='subcategory_edit'),
+	path('accounting/subcategory/<int:pk>/delete/', views.SubcategoryDeleteView.as_view(), name='subcategory_delete'),
+	
+		#Places urls
+	path('accounting/places/', views.PlacesListView.as_view(), name='places_list'),
+	path('accounting/place/create/', views.PlaceCreateView.as_view(), name='place_create'),
+	path('accounting/place/<int:pk>/edit/', views.PlaceEditView.as_view(), name='place_edit'),
+	path('accounting/place/<int:pk>/delete/', views.PlaceDeleteView.as_view(), name='place_delete'),
+	
+	#Statistics urls
+	#path('statistics', name='statistics'),
+	
+	#Planning urls
+	#path('planning', name='planning'),
 ]
--- finance/views.py
+++ finance/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from .models import Transaction, Account, Currency, Type, Category, Subcategory, Place
 
 # Create your views here.
+#Transactions views
 class TransactionsListView(generic.ListView):
 	model = Transaction
 	template_name = 'finance/transaction/transactions_list.html'
@@ -26,3 +27,29 @@ class TransactionDeleteView(generic.DeleteView):
 	model = Transaction
 	template_name = 'finance/transaction/transaction_confirm_delete.html'
 	success_url = reverse_lazy('finance:transactions_list')
+	
+#Accounts views	
+	
+class AccountsListView(generic.ListView):
+	model = Account
+	template_name = 'finance/account/accounts_list.html'
+	
+class AccountDetailView(generic.DetailView):
+	model = Account
+	template_name = 'finance/account/account_detail.html'
+	
+class AccountCreateView(generic.CreateView):
+	model = Account
+	fields = '__all__'
+	template_name = 'finance/account/account_create.html'
+	
+class AccountEditView(generic.UpdateView):
+	model = Account
+	fields = '__all__'
+	template_name = 'finance/account/account_edit.html'
+	
+class AccountDeleteView(generic.DeleteView):
+	model = Account
+	template_name = 'finance/account/account_confirm_delete.html'
+	success_url = reverse_lazy('finance:accounts_list')
+	
