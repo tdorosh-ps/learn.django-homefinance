39c7f9301ccc96ac69536bafed390e311e73c5f9
tdorosh <tdtechmail@ukr.net>
2019-05-21 20:05:23 +0300

    install django-debug-toolbar

--- home_finance/settings/development.py
+++ home_finance/settings/development.py
@@ -1,5 +1,8 @@
 from .base import *
 
 DEBUG = True
+INTERNAL_IPS = ('127.0.0.1', 'localhost')
+INSTALLED_APPS += ['debug_toolbar',]
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
 
 ALLOWED_HOSTS = []
